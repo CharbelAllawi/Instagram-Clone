51bcc10bdb805cfc0f622e23ab208e1a040607b3
Charbel Allawi <charbelallawiwork@gmail.com>
2023-08-08 23:31:24 +0300

    Front <> Added Pages & CSS

--- instagram-frontend/src/App.js
+++ instagram-frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Authentication from './pages/Authentication';
+import Landing from './pages/Landing';
+import Following from './pages/Following';
+import Followers from './pages/Followers';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Authentication />}></Route>
+          <Route path='/landing' element={<Landing />}></Route>
+          <Route path='/following' element={<Following />}></Route>
+          <Route path='/followers' element={<Followers />}></Route>
 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
