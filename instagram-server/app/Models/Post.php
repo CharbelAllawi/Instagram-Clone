0ec5c766001f1066e6b73a0801d34a0c6e0be83c
Charbel Allawi <charbelallawiwork@gmail.com>
2023-08-08 22:47:33 +0300

    Front <> Initial Commit

--- instagram-server/app/Models/Post.php
+++ instagram-server/app/Models/Post.php
@@ -6,15 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  // Relationship: Post belongs to User
   public function user()
   {
     return $this->belongsTo(User::class);
-  }
-
-  // Relationship: Post can have many likes (not implemented, but you can add a Likes table)
-  public function likes()
-  {
-    return $this->hasMany(Like::class);
   }
 }
