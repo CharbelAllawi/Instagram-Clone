c0ff6bd00b694ccc13d30473167e0650d06a0da5
Charbel Allawi <charbelallawiwork@gmail.com>
2023-08-10 14:31:17 +0300

    Server <> Follow / Unfollow API

--- instagram-server/app/Http/Controllers/UserController.php
+++ instagram-server/app/Http/Controllers/UserController.php
@@ -10,4 +10,22 @@ use App\Models\Post;
 
 class UserController extends Controller
 {
+    public function followorunfollow(Request $request)
+    {
+        $user = JWTAuth::user();
+
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $followerId = $request->input('follower_id');
+
+        if ($user->following()->where('following_id', $followerId)->exists()) {
+            $user->following()->detach($followerId);
+            return response()->json(['message' => 'User unfollowed successfully']);
+        } else {
+            $user->following()->attach($followerId);
+            return response()->json(['message' => 'User followed successfully']);
+        }
+    }
 }
